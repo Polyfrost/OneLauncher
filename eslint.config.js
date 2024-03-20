7349482c2038e9c142dc8b6f0bb9dd0256ce8bf1
Pauline <git@ethanlibs.co>
2024-03-20 20:48:45 +0100

    format and update schema migrations

--- eslint.config.js
+++ eslint.config.js
@@ -5,4 +5,17 @@ import petal from '@flowr/eslint-config';
 export default petal({
 	typescript: true,
 	solid: true,
-});
+	gitignore: true,
+	ignores: [
+		'**/target',
+		'**/dist',
+		'**/types',
+		'**/cache',
+		'**/dist',
+		'**/.temp',
+		'**/*.svg',
+		'*.rs',
+		'pnpm-lock.yaml',
+		'./src-tauri/prisma/migrations/migration_lock.toml',
+	],
+}, { rules: { 'petal/consistent-list-newline': 'off' } });

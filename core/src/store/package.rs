101a92bccc93e45a61cd1b6d40e4b90363eed8c0
Pauline <git@ethanlibs.co>
2024-05-11 06:19:44 +0200

    🔨 chore: hh

--- core/src/store/package.rs
+++ core/src/store/package.rs
@@ -1,5 +1,7 @@
 //! Handlers for Mod metadata that can be displayed in a GUI mod list or exported as a mod pack
 
+#[allow(clippy::all)]
+
 use async_zip::tokio::read::fs::ZipFileReader;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};

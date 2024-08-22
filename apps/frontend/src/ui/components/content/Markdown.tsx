43661fda6f888ffd80095e91156fd29c281fd6c2
LynithDev <61880709+LynithDev@users.noreply.github.com>
2024-08-22 16:59:56 +0200

    fix: Minor fixes (Navbar isActive, Markdown click prevention, Markdown list styling)

--- apps/frontend/src/ui/components/content/Markdown.tsx
+++ apps/frontend/src/ui/components/content/Markdown.tsx
@@ -6,7 +6,18 @@ export type MarkdownProps = ParentProps & {
 	body: string;
 };
 
-// eslint-disable-next-line solid/no-innerhtml -- we use xss to ensure there is not any issues
-const Markdown = (props: MarkdownProps) => <div class={styles.markdown} innerHTML={renderHighlightedString(props.body)} />;
+function Markdown(props: MarkdownProps) {
+	return (
+		<div
+			class={styles.markdown}
+			// eslint-disable-next-line solid/no-innerhtml -- Should be sanitised properly
+			innerHTML={renderHighlightedString(props.body)}
+			onClick={(e) => {
+				e.stopImmediatePropagation();
+				e.preventDefault();
+			}}
+		/>
+	);
+}
 
 export default Markdown;

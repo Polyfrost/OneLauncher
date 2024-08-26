dc26a5e175b7d8990c013a82b835272199efe56a
Pauline <git@ethanlibs.co>
2024-08-26 21:13:40 +0200

    🐛 fix: migrate
    Signed-off-by: Pauline <git@ethanlibs.co>

--- packages/core/prisma/migrations/20240826182714_init/migration.sql
+++ packages/core/prisma/migrations/20240826182714_init/migration.sql
@@ -3,12 +3,14 @@ CREATE TABLE "settings" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT DEFAULT 0,
     "theme" TEXT NOT NULL DEFAULT 'dark',
     "hide_close_prompt" BOOLEAN NOT NULL DEFAULT true,
-    "disable_animations" BOOLEAN NOT NULL DEFAULT false,
+    "reduced_motion" BOOLEAN NOT NULL DEFAULT false,
     "disable_analytics" BOOLEAN NOT NULL DEFAULT false,
     "debug_mode" BOOLEAN NOT NULL DEFAULT false,
     "hide_on_launch" BOOLEAN NOT NULL DEFAULT false,
     "force_fullscreen" BOOLEAN NOT NULL DEFAULT false,
     "disable_discord" BOOLEAN NOT NULL DEFAULT false,
+    "release_channel" TEXT NOT NULL DEFAULT 'stable',
+    "show_news" BOOLEAN NOT NULL DEFAULT true,
     "custom_java_args" TEXT NOT NULL,
     "custom_env_args" TEXT NOT NULL,
     "max_async_io_operations" INTEGER NOT NULL DEFAULT 10,
@@ -37,11 +39,11 @@ CREATE TABLE "java_version" (
 );
 
 -- CreateTable
-CREATE TABLE "JavaProfile" (
+CREATE TABLE "java_profile" (
     "name" TEXT NOT NULL PRIMARY KEY,
     "system" BOOLEAN NOT NULL DEFAULT false,
     "java_id" TEXT,
-    CONSTRAINT "JavaProfile_java_id_fkey" FOREIGN KEY ("java_id") REFERENCES "java_version" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+    CONSTRAINT "java_profile_java_id_fkey" FOREIGN KEY ("java_id") REFERENCES "java_version" ("id") ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -57,7 +59,7 @@ CREATE TABLE "minecraft_user" (
 );
 
 -- CreateTable
-CREATE TABLE "Skin" (
+CREATE TABLE "skin" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "skin" BLOB NOT NULL
 );
@@ -86,7 +88,7 @@ CREATE TABLE "cluster" (
     "loader_version" TEXT DEFAULT 'stable',
     "group_id" INTEGER NOT NULL,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "modified_at" DATETIME NOT NULL,
+    "updated_at" DATETIME NOT NULL,
     "played_at" DATETIME,
     "overall_played" INTEGER NOT NULL DEFAULT 0,
     "recently_played" INTEGER NOT NULL DEFAULT 0,
@@ -101,11 +103,11 @@ CREATE TABLE "cluster" (
     "override_hook_pre" TEXT,
     "override_hook_wrapper" TEXT,
     "override_hook_post" TEXT,
-    CONSTRAINT "cluster_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "ClusterGroup" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "cluster_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "cluster_group" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "ClusterGroup" (
+CREATE TABLE "cluster_group" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "index" INTEGER NOT NULL
@@ -120,6 +122,67 @@ CREATE TABLE "process" (
     "cluster_path" TEXT NOT NULL,
     "post_exit" TEXT,
     CONSTRAINT "process_cluster_path_fkey" FOREIGN KEY ("cluster_path") REFERENCES "cluster" ("path") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "version_info_cache" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "version_info" BLOB NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "partial_version_info" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "version_info" BLOB NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "assets_index_cache" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "assets_index" BLOB NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "package_cache" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sha512" TEXT NOT NULL,
+    "file_name" TEXT NOT NULL,
+    "file_size" INTEGER NOT NULL,
+    "disabled" BOOLEAN NOT NULL,
+    "meta_id" TEXT NOT NULL,
+    CONSTRAINT "package_cache_meta_id_fkey" FOREIGN KEY ("meta_id") REFERENCES "package_metadata" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "package_metadata" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT,
+    "package_id" TEXT,
+    "version" TEXT,
+    "description" TEXT,
+    "authors" TEXT,
+    "loader" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "managed_mod_cache" (
+    "meta_id" TEXT NOT NULL PRIMARY KEY,
+    "project_id" INTEGER NOT NULL,
+    "file_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "version" TEXT NOT NULL,
+    "urlslug" TEXT NOT NULL,
+    "summary" TEXT NOT NULL,
+    "authors" TEXT NOT NULL,
+    "release_type" INTEGER NOT NULL,
+    "update_paths" TEXT NOT NULL,
+    "cached_at" DATETIME NOT NULL,
+    CONSTRAINT "managed_mod_cache_meta_id_fkey" FOREIGN KEY ("meta_id") REFERENCES "package_metadata" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
@@ -139,3 +202,6 @@ CREATE INDEX "process_cluster_path_idx" ON "process"("cluster_path");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "process_pid_key" ON "process"("pid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "package_cache_file_name_key" ON "package_cache"("file_name");

202c9f0e97ba93e19d2539ed68777f800f1876cc
Pauline <git@ethanlibs.co>
2024-08-26 13:49:07 +0200

    🐛 fix: github actions
    Signed-off-by: Pauline <git@ethanlibs.co>

--- apps/desktop/src/lib.rs
+++ apps/desktop/src/lib.rs
@@ -20,10 +20,10 @@ async fn initialize_state(handle: &tauri::AppHandle) -> api::Result<()> {
 	Ok(())
 }
 
+/// initializes the logger and runs the app. if the logger fails to initialize
+/// we panic because nothing else can be debugged once the logger fails.
+/// the only thing that can fail before the logger should be our [`tokio::main`] loop.
 pub async fn run() {
-	// initializes the logger and runs the app. if the logger fails to initialize
-	// we panic because nothing else can be debugged once the logger fails.
-	// the only thing that can fail before the logger should be our `tokio::main` loop.
 	let _log_guard = onelauncher::start_logger();
 	tracing::info!("initialized logger. loading onelauncher/tauri...");
 

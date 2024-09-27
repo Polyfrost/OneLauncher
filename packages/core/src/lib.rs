8c99ee1b1fc2c17cda343d9ffbea4255566f4040
Pauline <git@ethanlibs.co>
2024-09-27 06:59:27 +0200

    ♻️ refactor: clippy lints
    Signed-off-by: Pauline <git@ethanlibs.co>

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -2,43 +2,6 @@
 //!
 //! A library used as a core for our launcher and Rust APIs.
 
-#![warn(
-	clippy::all,
-	clippy::pedantic,
-	clippy::correctness,
-	clippy::perf,
-	clippy::style,
-	clippy::suspicious,
-	clippy::complexity,
-	clippy::nursery,
-	clippy::unwrap_used,
-	unused_qualifications,
-	rust_2018_idioms,
-	trivial_casts,
-	trivial_numeric_casts,
-	unused_allocation,
-	clippy::unnecessary_cast,
-	clippy::cast_lossless,
-	clippy::cast_possible_truncation,
-	clippy::cast_possible_wrap,
-	clippy::cast_sign_loss,
-	clippy::dbg_macro,
-	unused_import_braces,
-	clippy::deprecated_cfg_attr,
-	clippy::separated_literal_suffix,
-	deprecated
-)]
-#![forbid(deprecated_in_future)]
-#![deny(unused_must_use)]
-#![allow(
-	clippy::missing_errors_doc,
-	clippy::future_not_send,
-	clippy::module_name_repetitions,
-	clippy::struct_field_names,
-	clippy::cast_precision_loss,
-	clippy::significant_drop_tightening //tmp
-)]
-
 #[macro_use]
 pub mod utils;
 

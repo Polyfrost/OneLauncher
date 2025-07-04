176e6cb06280d8e86be9490ca3cea83f5e1709ed
Szedann <65607498+Szedann@users.noreply.github.com>
2025-07-04 18:00:50 +0200

    refactor: more actual tailwind

--- apps/onelauncher/frontend/src/components/overlay/Modal.tsx
+++ apps/onelauncher/frontend/src/components/overlay/Modal.tsx
@@ -2,17 +2,20 @@ import type { ClassNameString } from '@onelauncher/common';
 import type { DialogProps } from 'react-aria-components';
 import {
 	Dialog as AriaDialog,
+	Modal as AriaModal,
 	DialogTrigger,
-	Modal as ModalOverlay,
+	ModalOverlay,
 } from 'react-aria-components';
 
 interface ModalProps extends DialogProps, ClassNameString { }
 
 function Modal({ className, ...props }: ModalProps) {
 	return (
-		<ModalOverlay isDismissable>
-			<AriaDialog className="" {...props} />
+		<ModalOverlay className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4" isDismissable>
+			<AriaModal>
+				<AriaDialog className="" {...props} />
 
+			</AriaModal>
 		</ModalOverlay>
 	);
 }

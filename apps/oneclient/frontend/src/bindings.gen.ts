e20ea3c0a8aef6ca4bfb990d3f4bdcac4992ed6c
Jacob <admin@kath.lol>
2025-11-15 16:53:42 +0800

    fix: OneClient Cluster Mods Page

--- apps/oneclient/frontend/src/bindings.gen.ts
+++ apps/oneclient/frontend/src/bindings.gen.ts
@@ -299,15 +299,8 @@ export type VersionType =
  */
 "old_beta"
 
-const ARGS_MAP = { 'folders':'{"fromCluster":["folder_name"],"openCluster":["folder_name"]}', 'events':'{"ingress":["event"],"process":["event"],"message":["event"]}', 'core':'{"getLogByName":["id","name"],"writeSettings":["setting"],"searchPackages":["provider","query"],"getRunningProcessesByClusterId":["cluster_id"],"getGameVersions":[],"getClusterById":["id"],"uploadSkinBytes":["access_token","skin_data","image_format","skin_variant"],"readSettings":[],"getPackageVersions":["provider","slug","mc_version","loader","offset","limit"],"getClusters":[],"getScreenshots":["id"],"setDefaultUser":["uuid"],"removeCluster":["id"],"fetchLoggedInProfile":["access_token"],"convertUsernameUUID":["username_uuid"],"createCluster":["options"],"isClusterRunning":["cluster_id"],"installModpack":["modpack","cluster_id"],"fetchMinecraftProfile":["uuid"],"removeUser":["uuid"],"getGlobalProfile":[],"openMsaLogin":[],"getLoadersForVersion":["mc_version"],"removeCape":["access_token"],"killProcess":["pid"],"downloadPackage":["provider","package_id","version_id","cluster_id","skip_compatibility"],"getUsers":[],"updateClusterById":["id","request"],"launchCluster":["id","uuid"],"getLogs":["id"],"getRunningProcesses":[],"getProfileOrDefault":["name"],"updateClusterProfile":["name","profile"],"getPackage":["provider","slug"],"getMods":["id"],"getPackageBody":["provider","body"],"changeSkin":["access_token","skin_url","skin_variant"],"open":["input"],"changeCape":["access_token","cape_uuid"],"getDefaultUser":["fallback"],"getMultiplePackages":["provider","slugs"],"createSettingsProfile":["name"],"getWorlds":["id"],"getUsersFromAuthor":["provider","author"],"getUser":["uuid"]}', 'oneclient':'{"openDevTools":[],"getBundlesFor":["cluster_id"],"getVersions":[],"getClustersGroupedByMajor":[]}' }
-export type Router = { 'events': { ingress: (event: IngressPayload) => Promise<void>, 
-message: (event: MessagePayload) => Promise<void>, 
-process: (event: ProcessPayload) => Promise<void> },
-'oneclient': { openDevTools: () => Promise<void>, 
-getClustersGroupedByMajor: () => Promise<Partial<{ [key in number]: ClusterModel[] }>>, 
-getBundlesFor: (clusterId: number) => Promise<ModpackArchive[]>, 
-getVersions: () => Promise<OnlineClusterManifest> },
-'core': { getClusters: () => Promise<ClusterModel[]>, 
+const ARGS_MAP = { 'events':'{"ingress":["event"],"process":["event"],"message":["event"]}', 'oneclient':'{"openDevTools":[],"getBundlesFor":["cluster_id"],"getVersions":[],"getClustersGroupedByMajor":[]}', 'folders':'{"fromCluster":["folder_name"],"openCluster":["folder_name"]}', 'core':'{"getLogByName":["id","name"],"writeSettings":["setting"],"searchPackages":["provider","query"],"getRunningProcessesByClusterId":["cluster_id"],"getGameVersions":[],"getClusterById":["id"],"uploadSkinBytes":["access_token","skin_data","image_format","skin_variant"],"readSettings":[],"getPackageVersions":["provider","slug","mc_version","loader","offset","limit"],"getClusters":[],"getScreenshots":["id"],"setDefaultUser":["uuid"],"removeCluster":["id"],"fetchLoggedInProfile":["access_token"],"convertUsernameUUID":["username_uuid"],"createCluster":["options"],"isClusterRunning":["cluster_id"],"installModpack":["modpack","cluster_id"],"fetchMinecraftProfile":["uuid"],"removeUser":["uuid"],"getGlobalProfile":[],"openMsaLogin":[],"getLoadersForVersion":["mc_version"],"removeCape":["access_token"],"killProcess":["pid"],"downloadPackage":["provider","package_id","version_id","cluster_id","skip_compatibility"],"getUsers":[],"updateClusterById":["id","request"],"launchCluster":["id","uuid"],"getLogs":["id"],"getRunningProcesses":[],"getProfileOrDefault":["name"],"updateClusterProfile":["name","profile"],"getPackage":["provider","slug"],"getMods":["id"],"getPackageBody":["provider","body"],"changeSkin":["access_token","skin_url","skin_variant"],"open":["input"],"changeCape":["access_token","cape_uuid"],"getDefaultUser":["fallback"],"getMultiplePackages":["provider","slugs"],"createSettingsProfile":["name"],"getWorlds":["id"],"getUsersFromAuthor":["provider","author"],"getUser":["uuid"]}' }
+export type Router = { 'core': { getClusters: () => Promise<ClusterModel[]>, 
 getClusterById: (id: number) => Promise<ClusterModel | null>, 
 removeCluster: (id: number) => Promise<null>, 
 createCluster: (options: CreateCluster) => Promise<ClusterModel>, 
@@ -352,8 +345,15 @@ changeCape: (accessToken: string, capeUuid: string) => Promise<MojangFullPlayerP
 removeCape: (accessToken: string) => Promise<MojangFullPlayerProfile>, 
 convertUsernameUUID: (usernameUuid: string) => Promise<MowojangProfile>, 
 open: (input: string) => Promise<null> },
+'events': { ingress: (event: IngressPayload) => Promise<void>, 
+message: (event: MessagePayload) => Promise<void>, 
+process: (event: ProcessPayload) => Promise<void> },
 'folders': { fromCluster: (folderName: string) => Promise<string>, 
-openCluster: (folderName: string) => Promise<null> } };
+openCluster: (folderName: string) => Promise<null> },
+'oneclient': { openDevTools: () => Promise<void>, 
+getClustersGroupedByMajor: () => Promise<Partial<{ [key in number]: ClusterModel[] }>>, 
+getBundlesFor: (clusterId: number) => Promise<ModpackArchive[]>, 
+getVersions: () => Promise<OnlineClusterManifest> } };
 
 
 export type { InferCommandOutput }
--- apps/oneclient/frontend/src/routes/app/cluster/mods.tsx
+++ apps/oneclient/frontend/src/routes/app/cluster/mods.tsx
@@ -1,7 +1,9 @@
-import { ModList } from '@/components/Bundle';
+import type { ModCardContextApi } from '@/components/Bundle';
+import { ModCardContext, ModList } from '@/components/Bundle';
 import { bindings } from '@/main';
 import { useCommandSuspense } from '@onelauncher/common';
 import { createFileRoute } from '@tanstack/react-router';
+import { useMemo } from 'react';
 
 export const Route = createFileRoute('/app/cluster/mods')({
 	component: RouteComponent,
@@ -11,10 +13,19 @@ function RouteComponent() {
 	const { cluster } = Route.useRouteContext();
 	const { data: bundles } = useCommandSuspense(['getBundlesFor', cluster.id], () => bindings.oneclient.getBundlesFor(cluster.id));
 
+	const context = useMemo<ModCardContextApi>(() => ({
+		showModDownloadButton: true,
+	}), []);
+
 	if (bundles.length === 0)
 		return <p>No bundles found {cluster.name}</p>;
 
 	return (
-		<ModList bundles={bundles} cluster={cluster} showModDownload />
+		<ModCardContext.Provider value={context}>
+			<ModList
+				bundles={bundles}
+				cluster={cluster}
+			/>
+		</ModCardContext.Provider>
 	);
 }

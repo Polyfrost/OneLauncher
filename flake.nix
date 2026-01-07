75871b75605a902c905644c23aec6cabd739594d
Jacob <admin@kath.lol>
2026-01-07 14:53:42 +0800

    chore: update nix flake
    nix deving
    
    Co-authored-by: not a cow <104355555+not-a-cowfr@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -1,13 +1,78 @@
 {
   description = "Next-generation open source Minecraft launcher";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.crane.url = "github:ipetkov/crane";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
 
-  outputs = inputs@{ flake-parts, ... }:
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, rust-overlay, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-      perSystem = { config, pkgs, ... }: {};
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          pkgs' = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+        in
+        {
+          devShells.default = pkgs'.mkShell {
+            packages = with pkgs'; [
+              (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+
+              cargo-tauri
+              nodejs
+              pnpm
+
+              pkg-config
+              openssl
+
+              gobject-introspection
+              at-spi2-atk
+              atkmm
+              cairo
+              gdk-pixbuf
+              glib
+              gtk3
+              harfbuzz
+              librsvg
+              libsoup_3
+              pango
+              webkitgtk_4_1
+            ];
+
+            shellHook = ''
+              pnpm i
+              pnpm prep
+            '';
+          };
+        };
     };
 }

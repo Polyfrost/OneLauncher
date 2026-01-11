9220ed023d53d6c205b6d7211651b72f334369a3
Jacob <kathund@proton.me>
2026-01-11 21:05:22 +0800

    fix(OneClient): building (#476)

--- apps/oneclient/frontend/src/types/global.d.ts
+++ apps/oneclient/frontend/src/types/global.d.ts
@@ -1,7 +1,7 @@
 import type { LauncherError } from '@/bindings.gen';
 import type { bindings } from '@/main';
 
-export type BindingCommands = (keyof typeof bindings.core) | (keyof typeof bindings.oneclient);
+export type BindingCommands = (keyof typeof bindings)[keyof typeof bindings];
 
 declare module '@onelauncher/common' {
 	interface Register {
